15bc72a3165fb6c09bc6d56f8ab105f4b0af55e5
srikarthik09 <79143050+srikarthik09@users.noreply.github.com>
2021-05-29 11:31:38 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -14,4 +14,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "a92315fc-a285-449b-8d7e-0b344381f057"
+  client_id       = "73486022-9c20-4276-9e25-f501f9544ac0"
+  client_secret   = "V-57gNVURSeb~B5MY7.89q.~SM53aI8w47"
+  tenant_id       = "e8d21255-9d63-4de2-8174-173cbdedfac9"
 }
