c905cd7b8ef3803bcff94e1b77663d2bbcf6fd59
srikarthik09 <79143050+srikarthik09@users.noreply.github.com>
2021-05-29 11:54:03 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
 
   subscription_id = "a92315fc-a285-449b-8d7e-0b344381f057"
   client_id       = "b2a76d8f-3ce9-4fa0-a4ab-c8d20821970c"
-  client_secret   = "V-57gNVURSeb~B5MY7.89q.~SM53aI8w47"
+  client_secret   = "w7v.N5vZf4P9n-wF760_IwU-2UndI3bcdi"
   tenant_id       = "e8d21255-9d63-4de2-8174-173cbdedfac9"
 }
